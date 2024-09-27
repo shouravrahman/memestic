cc92d26d6c639665dd0428b6a3674fd0d8cf3e95
shourav <shouravrahman006@gmail.com>
2024-09-27 07:07:08 +0600

    initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,61 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import { IKImage, IKUpload, ImageKitProvider } from "imagekitio-next";
+import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props";
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT as string;
+const authenticator = async () => {
+	try {
+		const response = await fetch("http://localhost:3000/api/auth");
+
+		if (!response.ok) {
+			const errorText = await response.text();
+			throw new Error(
+				`Request failed with status ${response.status}: ${errorText}`
+			);
+		}
+
+		const data = await response.json();
+		const { signature, expire, token } = data;
+		return { signature, expire, token };
+	} catch (error: any) {
+		throw new Error(`Authentication request failed: ${error.message}`);
+	}
+};
+
+const onError = (err: any) => {
+	console.log("Error", err);
+};
+
+const onSuccess = (res: IKUploadResponse) => {
+	console.log("Success", res);
+};
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<div>
+			<Button>Upload</Button>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+			<ImageKitProvider
+				publicKey={publicKey}
+				urlEndpoint={urlEndpoint}
+				authenticator={authenticator}
+			>
+				<IKImage
+					path="Expanding-Brain.jpg"
+					width={400}
+					height={400}
+					alt="Alt text"
+				/>
+				<div>
+					<h2>File upload</h2>
+					<IKUpload
+						fileName="test-upload.png"
+						onError={onError}
+						onSuccess={onSuccess}
+					/>
+				</div>
+			</ImageKitProvider>
+		</div>
+	);
 }
